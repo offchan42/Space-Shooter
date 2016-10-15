ff9be259540a046fbe2b93a7aeba371d00ef4ce0
Chanchana Sornsoontorn <off9955555@gmail.com>
2016-10-16 03:34:44 +0700

    Destroy explosion when its lifetime exceeds 2 seconds

---
+++ Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class DestroyByTime : MonoBehaviour
+{
+    public float lifeTime = 2.0f;
+
+    private void Awake()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+}
