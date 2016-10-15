beb44e12f61fc1f2b77a6054a753fe83f49b343e
Chanchana Sornsoontorn <off9955555@gmail.com>
2016-10-16 02:45:53 +0700

    Add explosion effect to the script and make Asteroid a prefab

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -2,12 +2,20 @@
 
 public class DestroyByContact : MonoBehaviour
 {
+    public GameObject explosionPrefab;
+    public GameObject playerExplosionPrefab;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Boundary"))
         {
             return;
         }
+        if (other.CompareTag("Player"))
+        {
+            Instantiate(playerExplosionPrefab, other.transform.position, other.transform.rotation);
+        }
+        Instantiate(explosionPrefab, transform.position, transform.rotation);
         Destroy(other.gameObject);
         Destroy(gameObject);
     }
