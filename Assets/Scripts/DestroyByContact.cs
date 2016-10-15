e60808fe8b2cfa3d44b95a2149e940d4fdab6a25
Chanchana Sornsoontorn <off9955555@gmail.com>
2016-10-16 02:35:07 +0700

    Add tag "Boundary" and make asteroid destructive

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Boundary"))
+        {
+            return;
+        }
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
