b201d78dfa3121054871159d98f50cd1d0dcff29
Chanchana Sornsoontorn <off9955555@gmail.com>
2016-10-16 03:07:32 +0700

    Add GameController to spawn hazard (asteroids)

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject hazardPrefab;
+    public Vector3 spawnValues;
+
+    public void SpawnHazard()
+    {
+        var spawnPos = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+        Instantiate(hazardPrefab, spawnPos, Quaternion.identity);
+    }
+
+
+    private void Awake()
+    {
+        SpawnHazard();
+    }
+}
