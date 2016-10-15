29ca0e376db941e8c8471b82b4ded34bb811a3d9
Chanchana Sornsoontorn <off9955555@gmail.com>
2016-10-16 03:34:09 +0700

    Add SpawnWaves() and delay variables

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,9 +1,14 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
     public GameObject hazardPrefab;
     public Vector3 spawnValues;
+    public int waveSize = 5;
+    public float startDelay = 1.0f;
+    public float hazardDelay = 0.5f;
+    public float waveDelay = 3.0f;
 
     public void SpawnHazard()
     {
@@ -11,9 +16,22 @@ public class GameController : MonoBehaviour
         Instantiate(hazardPrefab, spawnPos, Quaternion.identity);
     }
 
+    public IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startDelay);
+        while (true)
+        {
+            for (var i = 0; i < waveSize; i++)
+            {
+                SpawnHazard();
+                yield return new WaitForSeconds(hazardDelay);
+            }
+            yield return new WaitForSeconds(waveDelay);
+        }
+    }
 
     private void Awake()
     {
-        SpawnHazard();
+        StartCoroutine(SpawnWaves());
     }
 }
