0ef04a04683ef4d1b8f2ba9010d09ba4ea4a835d
Chanchana Sornsoontorn <off9955555@gmail.com>
2016-10-16 00:03:06 +0700

    Add PlayerController script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        var velocity = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.velocity = velocity;
+    }
+}
