4e394d7f90cd01d30125679a02770d4c5d6583bf
Chanchana Sornsoontorn <off9955555@gmail.com>
2016-10-16 02:20:50 +0700

    Add an Asteroid object and RandomRotator script to it

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class RandomRotator : MonoBehaviour
+{
+    public float tumble = 5.0f;
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.angularVelocity = Random.insideUnitSphere*tumble;
+    }
+}
