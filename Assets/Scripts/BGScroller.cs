894e5dc683155bd459396034374ebf7eb8f3206c
Chanchana Sornsoontorn <off9955555@gmail.com>
2016-10-16 19:49:47 +0700

    Add background scroller to the scene Duplicate the background quad and scroll it down with speed=0.25, tileSize=30

---
+++ Assets/Scripts/BGScroller.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class BGScroller : MonoBehaviour
+{
+    public float scrollSpeed;
+    public float tileSizeZ;
+
+    private void Update()
+    {
+        Vector3 newPos = transform.position;
+        newPos.z = -Mathf.Repeat(Time.time*scrollSpeed, tileSizeZ);
+        transform.position = newPos;
+    }
+}
